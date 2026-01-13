4997e476e1f5e49830105b962da445a070788399
Uwiagod <sensigod466@gmail.com>
2026-01-13 12:31:11 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-    if (typeof particlesJS !== 'undefined') {
+function initParticles() {
+    if (window.particlesJS) {
         particlesJS("particles-js", {
             "particles": {
-                "number": { "value": 80, "density": { "enable": true, "value_area": 800 } },
+                "number": { "value": 60 },
                 "color": { "value": "#ff0000" },
                 "shape": { "type": "edge" },
-                "opacity": { "value": 0.5, "random": true },
-                "size": { "value": 2, "random": true },
-                "line_linked": { "enable": true, "distance": 140, "color": "#ff0000", "opacity": 0.2, "width": 1 },
-                "move": { "enable": true, "speed": 4, "direction": "none", "random": true, "out_mode": "out" }
-            },
-            "interactivity": {
-                "detect_on": "canvas",
-                "events": { "onhover": { "enable": true, "mode": "repulse" } }
+                "opacity": { "value": 0.4 },
+                "size": { "value": 2 },
+                "line_linked": { "enable": true, "distance": 150, "color": "#ff0000", "opacity": 0.2, "width": 1 },
+                "move": { "enable": true, "speed": 2 }
             },
             "retina_detect": true
         });
-        console.log("BENX!TER CORE: LOADED");
-    } else {
-        console.warn("Retrying particles load...");
+        console.log("BENX Particles Active");
     }
-});
+}
+
+// Try to init every 500ms until library is ready
+let checkLoad = setInterval(() => {
+    if (window.particlesJS) {
+        initParticles();
+        clearInterval(checkLoad);
+    }
+}, 500);
