5f99b4b0bd64a47f829d0f88fca41f68ae17daa2
Uwiagod <sensigod466@gmail.com>
2026-01-13 12:52:37 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 window.onload = function() {
-    particlesJS("particles-js", {
-        "particles": {
-            "number": { "value": 50, "density": { "enable": true, "value_area": 800 } },
-            "color": { "value": "#ff0000" },
-            "shape": { "type": "edge" },
-            "opacity": { "value": 0.4 },
-            "size": { "value": 2 },
-            "line_linked": { "enable": true, "distance": 150, "color": "#ff0000", "opacity": 0.1, "width": 1 },
-            "move": { "enable": true, "speed": 1.5, "direction": "none", "random": true }
-        },
-        "retina_detect": true
-    });
+    if (typeof particlesJS !== 'undefined') {
+        particlesJS("particles-js", {
+            "particles": {
+                "number": { "value": 80 },
+                "color": { "value": "#ff0000" },
+                "shape": { "type": "edge" },
+                "opacity": { "value": 0.5 },
+                "size": { "value": 2 },
+                "line_linked": { "enable": true, "distance": 150, "color": "#ff0000", "opacity": 0.2, "width": 1 },
+                "move": { "enable": true, "speed": 3 }
+            }
+        });
+    }
+    console.log("BENX CORE INITIALIZED");
 };
