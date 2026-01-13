fe6c49572a9570d880e6347aefc82841af333804
Uwiagod <sensigod466@gmail.com>
2026-01-13 13:26:17 +0600

    Update script.js

--- script.js
+++ script.js
@@ -17,36 +17,35 @@ window.onload = function() {
                 loader.style.display = "none";
                 mainUI.style.display = "block";
                 
-                // ADVANCED SCANNER LOGIC
-                document.getElementById('os-info').innerText = navigator.platform.toUpperCase();
-                document.getElementById('vendor-info').innerText = navigator.vendor.split(' ')[0] || "PC USER";
-                document.getElementById('core-info').innerText = (navigator.hardwareConcurrency || "X") + " THREADS";
-                document.getElementById('ram-info').innerText = (navigator.deviceMemory || "8") + " GB";
-                
-                // CPU & GPU DETECTION
-                let canvas = document.createElement("canvas");
-                let gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-                if (gl) {
-                    let debugInfo = gl.getExtension("WEBGL_debug_renderer_info");
-                    let renderer = gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL);
-                    document.getElementById('gpu-info').innerText = renderer.split(' ').pop();
-                    document.getElementById('cpu-info').innerText = renderer.split(' ')[1] || "GENERIC";
-                }
+                // DATA SCANNER
+                try {
+                    document.getElementById('os-info').innerText = navigator.platform.toUpperCase();
+                    document.getElementById('vendor-info').innerText = navigator.vendor.split(' ')[0] || "USER";
+                    document.getElementById('core-info').innerText = navigator.hardwareConcurrency + " THREADS";
+                    document.getElementById('ram-info').innerText = (navigator.deviceMemory || "8") + " GB";
+                    
+                    let canvas = document.createElement("canvas");
+                    let gl = canvas.getContext("webgl");
+                    if (gl) {
+                        let debug = gl.getExtension("WEBGL_debug_renderer_info");
+                        let renderer = gl.getParameter(debug.UNMASKED_RENDERER_WEBGL);
+                        document.getElementById('gpu-info').innerText = renderer.split(' ').pop();
+                        document.getElementById('cpu-info').innerText = "DETECTED";
+                    }
+                } catch(e) { console.log("Scanner Error"); }
 
-                if (window.particlesJS) {
-                    particlesJS("particles-js", {
-                        "particles": {
-                            "number": { "value": 80 },
-                            "color": { "value": "#ff0000" },
-                            "shape": { "type": "edge" },
-                            "opacity": { "value": 0.5 },
-                            "size": { "value": 2 },
-                            "line_linked": { "enable": true, "distance": 150, "color": "#ff0000", "opacity": 0.2, "width": 1 },
-                            "move": { "enable": true, "speed": 2 }
-                        }
-                    });
-                }
-            }, 400);
+                particlesJS("particles-js", {
+                    "particles": {
+                        "number": { "value": 70 },
+                        "color": { "value": "#ff0000" },
+                        "shape": { "type": "edge" },
+                        "opacity": { "value": 0.5 },
+                        "size": { "value": 2 },
+                        "line_linked": { "enable": true, "distance": 150, "color": "#ff0000", "opacity": 0.2, "width": 1 },
+                        "move": { "enable": true, "speed": 2 }
+                    }
+                });
+            }, 500);
         }
     }, 25);
 };
