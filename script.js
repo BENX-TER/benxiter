be756a81b527910d76a5deb55dce8586913d5a7f
Uwiagod <sensigod466@gmail.com>
2026-01-13 12:12:04 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 particlesJS("particles-js", {
   "particles": {
-    "number": { "value": 70 },
+    "number": { "value": 40 }, // Fewer particles = Cleaner UI
     "color": { "value": "#ff0000" },
     "shape": { "type": "circle" },
-    "opacity": { "value": 0.5 },
+    "opacity": { "value": 0.3 },
     "size": { "value": 2 },
-    "line_linked": { "enable": true, "distance": 150, "color": "#ff0000", "opacity": 0.2, "width": 1 },
-    "move": { "enable": true, "speed": 2 }
+    "line_linked": { "enable": true, "distance": 200, "color": "#ff0000", "opacity": 0.15, "width": 1 },
+    "move": { "enable": true, "speed": 1 }
   }
 });
