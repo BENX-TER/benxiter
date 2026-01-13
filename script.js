401cf0ca369fb65b64c2d4691e82866dae3b1585
Uwiagod <sensigod466@gmail.com>
2026-01-13 12:08:04 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": { "value": 70 },
+    "color": { "value": "#ff0000" },
+    "shape": { "type": "circle" },
+    "opacity": { "value": 0.5 },
+    "size": { "value": 2 },
+    "line_linked": { "enable": true, "distance": 150, "color": "#ff0000", "opacity": 0.2, "width": 1 },
+    "move": { "enable": true, "speed": 2 }
+  }
+});
