8066a2a6e13ce306e17592a64f8d6720eba5951b
Uwiagod <sensigod466@gmail.com>
2026-01-13 13:05:24 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 window.onload = function() {
     let count = 0;
-    let counter = document.getElementById("count");
-    let progBar = document.getElementById("progress-bar");
+    let counterText = document.getElementById("count");
+    let bar = document.getElementById("progress-bar");
     let loader = document.getElementById("benx-loader");
     let mainUI = document.getElementById("main-ui");
 
-    let id = setInterval(() => {
+    let loadInterval = setInterval(() => {
         count++;
-        if (counter) counter.innerHTML = count;
-        
-        // Circle math: 440 is the circumference
+        if (counterText) counterText.innerHTML = count;
         let offset = 440 - (440 * count) / 100;
-        if (progBar) progBar.style.strokeDashoffset = offset;
+        if (bar) bar.style.strokeDashoffset = offset;
 
         if(count >= 100) {
-            clearInterval(id);
+            clearInterval(loadInterval);
             setTimeout(() => {
                 loader.style.display = "none";
                 mainUI.style.display = "block";
-                // Start Particles after loader finishes
                 if (window.particlesJS) {
                     particlesJS("particles-js", {
                         "particles": {
@@ -32,7 +29,7 @@ window.onload = function() {
                         }
                     });
                 }
-            }, 500);
+            }, 400);
         }
-    }, 30);
+    }, 25);
 };
