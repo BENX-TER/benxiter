c4869702707811ac2dbeb45d5dab22ccbea75709
Uwiagod <sensigod466@gmail.com>
2026-01-13 12:36:02 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
-function initParticles() {
-    if (window.particlesJS) {
-        particlesJS("particles-js", {
-            "particles": {
-                "number": { "value": 60 },
-                "color": { "value": "#ff0000" },
-                "shape": { "type": "edge" },
-                "opacity": { "value": 0.4 },
-                "size": { "value": 2 },
-                "line_linked": { "enable": true, "distance": 150, "color": "#ff0000", "opacity": 0.2, "width": 1 },
-                "move": { "enable": true, "speed": 2 }
-            },
-            "retina_detect": true
-        });
-        console.log("BENX Particles Active");
+document.addEventListener("DOMContentLoaded", function() {
+    function startParticles() {
+        if (window.particlesJS) {
+            particlesJS("particles-js", {
+                "particles": {
+                    "number": { "value": 50, "density": { "enable": true, "value_area": 800 } },
+                    "color": { "value": "#ff0000" },
+                    "shape": { "type": "edge" },
+                    "opacity": { "value": 0.3 },
+                    "size": { "value": 2 },
+                    "line_linked": { "enable": true, "distance": 150, "color": "#ff0000", "opacity": 0.1, "width": 1 },
+                    "move": { "enable": true, "speed": 2 }
+                },
+                "retina_detect": true
+            });
+            console.log("BENX_SYSTEM: Particles Online");
+        }
     }
-}
 
-// Try to init every 500ms until library is ready
-let checkLoad = setInterval(() => {
-    if (window.particlesJS) {
-        initParticles();
-        clearInterval(checkLoad);
-    }
-}, 500);
+    // Delay start slightly to ensure library is parsed
+    setTimeout(startParticles, 300);
+});
