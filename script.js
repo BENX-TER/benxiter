4e571dd3b362f00a481ebdf51b2d19a801f4f6f6
Uwiagod <sensigod466@gmail.com>
2026-01-13 12:49:23 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 window.onload = function() {
     particlesJS("particles-js", {
         "particles": {
-            "number": { "value": 60 },
+            "number": { "value": 50, "density": { "enable": true, "value_area": 800 } },
             "color": { "value": "#ff0000" },
             "shape": { "type": "edge" },
-            "opacity": { "value": 0.3 },
+            "opacity": { "value": 0.4 },
             "size": { "value": 2 },
             "line_linked": { "enable": true, "distance": 150, "color": "#ff0000", "opacity": 0.1, "width": 1 },
-            "move": { "enable": true, "speed": 2 }
-        }
+            "move": { "enable": true, "speed": 1.5, "direction": "none", "random": true }
+        },
+        "retina_detect": true
     });
 };
