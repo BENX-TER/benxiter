924156a1aee97d039d314dbf007643573567e36c
Uwiagod <sensigod466@gmail.com>
2026-01-13 13:30:19 +0600

    Update script.js

--- script.js
+++ script.js
@@ -14,38 +14,40 @@ window.onload = function() {
         if(count >= 100) {
             clearInterval(loadInterval);
             setTimeout(() => {
-                loader.style.display = "none";
-                mainUI.style.display = "block";
-                
-                // DATA SCANNER
-                try {
-                    document.getElementById('os-info').innerText = navigator.platform.toUpperCase();
-                    document.getElementById('vendor-info').innerText = navigator.vendor.split(' ')[0] || "USER";
-                    document.getElementById('core-info').innerText = navigator.hardwareConcurrency + " THREADS";
-                    document.getElementById('ram-info').innerText = (navigator.deviceMemory || "8") + " GB";
-                    
-                    let canvas = document.createElement("canvas");
-                    let gl = canvas.getContext("webgl");
-                    if (gl) {
-                        let debug = gl.getExtension("WEBGL_debug_renderer_info");
-                        let renderer = gl.getParameter(debug.UNMASKED_RENDERER_WEBGL);
-                        document.getElementById('gpu-info').innerText = renderer.split(' ').pop();
-                        document.getElementById('cpu-info').innerText = "DETECTED";
-                    }
-                } catch(e) { console.log("Scanner Error"); }
-
-                particlesJS("particles-js", {
-                    "particles": {
-                        "number": { "value": 70 },
-                        "color": { "value": "#ff0000" },
-                        "shape": { "type": "edge" },
-                        "opacity": { "value": 0.5 },
-                        "size": { "value": 2 },
-                        "line_linked": { "enable": true, "distance": 150, "color": "#ff0000", "opacity": 0.2, "width": 1 },
-                        "move": { "enable": true, "speed": 2 }
-                    }
-                });
+                loader.style.opacity = "0";
+                setTimeout(() => {
+                    loader.style.display = "none";
+                    mainUI.style.display = "block";
+                    startHardwareScan();
+                }, 500);
             }, 500);
         }
-    }, 25);
+    }, 20);
+
+    function startHardwareScan() {
+        document.getElementById('os-info').innerText = navigator.platform;
+        document.getElementById('cpu-info').innerText = navigator.hardwareConcurrency + " CORES";
+        document.getElementById('ram-info').innerText = (navigator.deviceMemory || "8") + " GB";
+        
+        // GPU Detection
+        let canvas = document.createElement("canvas");
+        let gl = canvas.getContext("webgl");
+        if (gl) {
+            let debug = gl.getExtension("WEBGL_debug_renderer_info");
+            let renderer = gl.getParameter(debug.UNMASKED_RENDERER_WEBGL);
+            document.getElementById('gpu-info').innerText = renderer.split(' ').pop();
+        }
+
+        particlesJS("particles-js", {
+            "particles": {
+                "number": { "value": 60 },
+                "color": { "value": "#ff0000" },
+                "shape": { "type": "circle" },
+                "opacity": { "value": 0.3 },
+                "size": { "value": 2 },
+                "line_linked": { "enable": true, "distance": 150, "color": "#ff0000", "opacity": 0.2, "width": 1 },
+                "move": { "enable": true, "speed": 1 }
+            }
+        });
+    }
 };
